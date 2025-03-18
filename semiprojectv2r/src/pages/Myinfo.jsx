5de6ffc160167157fc821257c40ac4fe6ac0bb6e
jademat <01088049128a@gmail.com>
2025-03-18 12:44:37 +0900

    Myinfo 출력

---
+++ semiprojectv2r/src/pages/Myinfo.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Myinfo = () => {
+    return (
+        <main id="content">
+            <h2>회원정보</h2>
+            <div id="myinfo">
+                <table className="table table-bordered mb-0">
+                    <colgroup>
+                        <col style={{width:"30%"}}/>
+                        <col/>
+                    </colgroup>
+                    <tbody>
+                    <tr>
+                        <td>아이디</td>
+                        <td><span >abc123</span></td>
+                    </tr>
+                    <tr>
+                        <td>이름</td>
+                        <td><span >abc123</span></td>
+                    </tr>
+                    <tr>
+                        <td>이메일</td>
+                        <td><span >abc123</span></td>
+                    </tr>
+                    <tr>
+                        <td>가입일</td>
+                        <td><span>2025-03-18 12:35:35</span>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </main>
+    )
+}
+
+export default Myinfo;
