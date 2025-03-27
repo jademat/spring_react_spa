008a7d8a6b4d76640de2129a36d0317063c94d45
jademat <01088049128a@gmail.com>
2025-03-27 09:31:25 +0900

    react JSX 로 게시판 목록 출력

--- semiprojectv2r/src/pages/BoardList.jsx
+++ semiprojectv2r/src/pages/BoardList.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 import "../styles/board.css";
 // Main 함수 컴포넌트 정의
 const BoardList = () => {
 
+    const [boardData, setBoardData] = useState({});
+
+    // react에서 부수작업 side effect을 수행하기 위한 hook
+    // 부수작업 : 데이터 가져오기, dom 조작, 로그
+    useEffect(() => {
+        fetch(`http://localhost:8080/api/board/list`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setBoardData(data);
+            })
+            .catch(err => console.log('오류발생', err));
+    }, []);
+
     const goBoardWrite = () => {
-        location.href= '/board/write';
+        location.href = '/board/write';
     };
 
     return (
@@ -27,7 +41,7 @@ const BoardList = () => {
                         <div className="d-flex align-items-center gap-3">
                             <select className="form-select" style={{width: "120px"}} id="findtype" name="findtype"
                                     defaultValue="title">
-                                <option value="title" >제목</option>
+                                <option value="title">제목</option>
                                 <option value="userid">작성자</option>
                                 <option value="contents">내용</option>
                             </select>
@@ -39,7 +53,7 @@ const BoardList = () => {
                     </td>
                     <td colSpan="3" className="text-end">
                         <button type="button" className="btn btn-primary" id="newbdbtn"
-                        onClick={goBoardWrite}><i className="fas fa-pen"/> 글쓰기
+                                onClick={goBoardWrite}><i className="fas fa-pen"/> 글쓰기
                         </button>
                     </td>
                 </tr>
@@ -54,304 +68,41 @@ const BoardList = () => {
                 </thead>
 
                 <tbody>
-                <tr>
-                    <td>3939</td>
-                    <td><a href="/board/view?bno=3939">fdsfasd</a> </td>
-                    <td>abc123</td>
-                    <td>2025-03-05</td>
-                    <td>0</td>
-                    <td>27</td>
-                </tr>
-                <tr>
-                    <td>3938</td>
-                    <td><a href="/board/view?bno=3938">AMD, 차세대 RDNA 4 기반 라데온 RX 9000 공개…‘AI 게이밍 진화’</a> </td>
-                    <td>냐옹냐옹</td>
-                    <td>2025-03-04</td>
-                    <td>0</td>
-                    <td>53</td>
-                </tr>
-                <tr>
-                    <td>3937</td>
-                    <td><a href="/board/view?bno=3937">&quot;아빠, 곧 나오는데 참고하라고&quot;… 닌텐도 스위치2 출시 윤곽</a> </td>
-                    <td>987xyz</td>
-                    <td>2025-03-04</td>
-                    <td>0</td>
-                    <td>1</td>
-                </tr>
-                <tr>
-                    <td>3936</td>
-                    <td><a href="/board/view?bno=3936">다이소 결국 일냈다…“3만원→3천원 영양제 품절사태” 발칵 뒤집힌 약국</a> </td>
-                    <td>abc123</td>
-                    <td>2025-03-04</td>
-                    <td>0</td>
-                    <td>10</td>
-                </tr>
-                <tr>
-                    <td>3935</td>
-                    <td><a href="/board/view?bno=3935">AMD, 차세대 RDNA 4 기반 라데온 RX 9000 공개…‘AI 게이밍 진화’</a> </td>
-                    <td>냐옹냐옹</td>
-                    <td>2025-03-04</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
-                <tr>
-                    <td>3934</td>
-                    <td><a href="/board/view?bno=3934">&quot;아빠, 곧 나오는데 참고하라고&quot;… 닌텐도 스위치2 출시 윤곽</a> </td>
-                    <td>987xyz</td>
-                    <td>2025-03-04</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
-                <tr>
-                    <td>3933</td>
-                    <td><a href="/board/view?bno=3933">다이소 결국 일냈다…“3만원→3천원 영양제 품절사태” 발칵 뒤집힌 약국</a> </td>
-                    <td>abc123</td>
-                    <td>2025-03-04</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
-                <tr>
-                    <td>3932</td>
-                    <td><a href="/board/view?bno=3932">AMD, 차세대 RDNA 4 기반 라데온 RX 9000 공개…‘AI 게이밍 진화’</a> </td>
-                    <td>냐옹냐옹</td>
-                    <td>2025-03-04</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
-                <tr>
-                    <td>3931</td>
-                    <td><a href="/board/view?bno=3931">&quot;아빠, 곧 나오는데 참고하라고&quot;… 닌텐도 스위치2 출시 윤곽</a> </td>
-                    <td>987xyz</td>
-                    <td>2025-03-04</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
-                <tr>
-                    <td>3930</td>
-                    <td><a href="/board/view?bno=3930">다이소 결국 일냈다…“3만원→3천원 영양제 품절사태” 발칵 뒤집힌 약국</a> </td>
-                    <td>abc123</td>
-                    <td>2025-03-04</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
-                <tr>
-                    <td>3929</td>
-                    <td><a href="/board/view?bno=3929">AMD, 차세대 RDNA 4 기반 라데온 RX 9000 공개…‘AI 게이밍 진화’</a> </td>
-                    <td>냐옹냐옹</td>
-                    <td>2025-03-04</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
-                <tr>
-                    <td>3928</td>
-                    <td><a href="/board/view?bno=3928">&quot;아빠, 곧 나오는데 참고하라고&quot;… 닌텐도 스위치2 출시 윤곽</a> </td>
-                    <td>987xyz</td>
-                    <td>2025-03-04</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
-                <tr>
-                    <td>3927</td>
-                    <td><a href="/board/view?bno=3927">다이소 결국 일냈다…“3만원→3천원 영양제 품절사태” 발칵 뒤집힌 약국</a> </td>
-                    <td>abc123</td>
-                    <td>2025-03-04</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
-                <tr>
-                    <td>3926</td>
-                    <td><a href="/board/view?bno=3926">AMD, 차세대 RDNA 4 기반 라데온 RX 9000 공개…‘AI 게이밍 진화’</a> </td>
-                    <td>냐옹냐옹</td>
-                    <td>2025-03-04</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
-                <tr>
-                    <td>3925</td>
-                    <td><a href="/board/view?bno=3925">&quot;아빠, 곧 나오는데 참고하라고&quot;… 닌텐도 스위치2 출시 윤곽</a> </td>
-                    <td>987xyz</td>
-                    <td>2025-03-04</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
-                <tr>
-                    <td>3924</td>
-                    <td><a href="/board/view?bno=3924">다이소 결국 일냈다…“3만원→3천원 영양제 품절사태” 발칵 뒤집힌 약국</a> </td>
-                    <td>abc123</td>
-                    <td>2025-03-04</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
-                <tr>
-                    <td>3923</td>
-                    <td><a href="/board/view?bno=3923">AMD, 차세대 RDNA 4 기반 라데온 RX 9000 공개…‘AI 게이밍 진화’</a> </td>
-                    <td>냐옹냐옹</td>
-                    <td>2025-03-04</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
-                <tr>
-                    <td>3922</td>
-                    <td><a href="/board/view?bno=3922">&quot;아빠, 곧 나오는데 참고하라고&quot;… 닌텐도 스위치2 출시 윤곽</a> </td>
-                    <td>987xyz</td>
-                    <td>2025-03-04</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
-                <tr>
-                    <td>3921</td>
-                    <td><a href="/board/view?bno=3921">다이소 결국 일냈다…“3만원→3천원 영양제 품절사태” 발칵 뒤집힌 약국</a> </td>
-                    <td>abc123</td>
-                    <td>2025-03-04</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
-                <tr>
-                    <td>3920</td>
-                    <td><a href="/board/view?bno=3920">AMD, 차세대 RDNA 4 기반 라데온 RX 9000 공개…‘AI 게이밍 진화’</a> </td>
-                    <td>냐옹냐옹</td>
-                    <td>2025-03-04</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
-                <tr>
-                    <td>3919</td>
-                    <td><a href="/board/view?bno=3919">&quot;아빠, 곧 나오는데 참고하라고&quot;… 닌텐도 스위치2 출시 윤곽</a> </td>
-                    <td>987xyz</td>
-                    <td>2025-03-04</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
-                <tr>
-                    <td>3918</td>
-                    <td><a href="/board/view?bno=3918">다이소 결국 일냈다…“3만원→3천원 영양제 품절사태” 발칵 뒤집힌 약국</a> </td>
-                    <td>abc123</td>
-                    <td>2025-03-04</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
-                <tr>
-                    <td>3917</td>
-                    <td><a href="/board/view?bno=3917">AMD, 차세대 RDNA 4 기반 라데온 RX 9000 공개…‘AI 게이밍 진화’</a> </td>
-                    <td>냐옹냐옹</td>
-                    <td>2025-03-04</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
-                <tr>
-                    <td>3916</td>
-                    <td><a href="/board/view?bno=3916">&quot;아빠, 곧 나오는데 참고하라고&quot;… 닌텐도 스위치2 출시 윤곽</a> </td>
-                    <td>987xyz</td>
-                    <td>2025-03-04</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
-                <tr>
-                    <td>3915</td>
-                    <td><a href="/board/view?bno=3915">다이소 결국 일냈다…“3만원→3천원 영양제 품절사태” 발칵 뒤집힌 약국</a> </td>
-                    <td>abc123</td>
-                    <td>2025-03-04</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
-                <tr>
-                    <td>3914</td>
-                    <td><a href="/board/view?bno=3914">AMD, 차세대 RDNA 4 기반 라데온 RX 9000 공개…‘AI 게이밍 진화’</a> </td>
-                    <td>냐옹냐옹</td>
-                    <td>2025-03-04</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
-                <tr>
-                    <td>3913</td>
-                    <td><a href="/board/view?bno=3913">&quot;아빠, 곧 나오는데 참고하라고&quot;… 닌텐도 스위치2 출시 윤곽</a> </td>
-                    <td>987xyz</td>
-                    <td>2025-03-04</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
-                <tr>
-                    <td>3912</td>
-                    <td><a href="/board/view?bno=3912">다이소 결국 일냈다…“3만원→3천원 영양제 품절사태” 발칵 뒤집힌 약국</a> </td>
-                    <td>abc123</td>
-                    <td>2025-03-04</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
-                <tr>
-                    <td>3911</td>
-                    <td><a href="/board/view?bno=3911">AMD, 차세대 RDNA 4 기반 라데온 RX 9000 공개…‘AI 게이밍 진화’</a> </td>
-                    <td>냐옹냐옹</td>
-                    <td>2025-03-04</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
-                <tr>
-                    <td>3910</td>
-                    <td><a href="/board/view?bno=3910">&quot;아빠, 곧 나오는데 참고하라고&quot;… 닌텐도 스위치2 출시 윤곽</a> </td>
-                    <td>987xyz</td>
-                    <td>2025-03-04</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
-                <tr>
-                    <td>3909</td>
-                    <td><a href="/board/view?bno=3909">다이소 결국 일냈다…“3만원→3천원 영양제 품절사태” 발칵 뒤집힌 약국</a> </td>
-                    <td>abc123</td>
-                    <td>2025-03-04</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
-                <tr>
-                    <td>3908</td>
-                    <td><a href="/board/view?bno=3908">AMD, 차세대 RDNA 4 기반 라데온 RX 9000 공개…‘AI 게이밍 진화’</a> </td>
-                    <td>냐옹냐옹</td>
-                    <td>2025-03-04</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
-                <tr>
-                    <td>3907</td>
-                    <td><a href="/board/view?bno=3907">&quot;아빠, 곧 나오는데 참고하라고&quot;… 닌텐도 스위치2 출시 윤곽</a> </td>
-                    <td>987xyz</td>
-                    <td>2025-03-04</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
-                <tr>
-                    <td>3906</td>
-                    <td><a href="/board/view?bno=3906">다이소 결국 일냈다…“3만원→3천원 영양제 품절사태” 발칵 뒤집힌 약국</a> </td>
-                    <td>abc123</td>
-                    <td>2025-03-04</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
-                <tr>
-                    <td>3905</td>
-                    <td><a href="/board/view?bno=3905">AMD, 차세대 RDNA 4 기반 라데온 RX 9000 공개…‘AI 게이밍 진화’</a> </td>
-                    <td>냐옹냐옹</td>
-                    <td>2025-03-04</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
+                {
+                    (!Array.isArray(boardData.bdlist) || boardData.bdlist.length === 0) ?
+                        <tr>
+                            <td colSpan="6">게시글이 없습니다.</td>
+                        </tr>
+                        :
+                        (boardData.bdlist.map(bd => (
+                            <tr key = {bd.bno}>
+                                <td>{bd.bno}</td>
+                                <td><a href="/board/view?bno=${bd.bno}">{bd.title}</a></td>
+                                <td>{bd.userid}</td>
+                                <td>{bd.regdate.substring(0, 10)}</td>
+                                <td>{bd.thumbs}</td>
+                                <td>{bd.views}</td>
+                            </tr>
+                        )))
+                }
                 </tbody>
 
                 <tfoot>
                 <tr>
                     <td colSpan="6">
                         <ul className="pagination">
-                            <li className="page-item" ><a  className="page-link">이전</a></li>
-                                <li> <a href="#" className="page-link">1</a></li>
-                                <li> <a href="#" className="page-link">2</a></li>
-                                <li> <a href="#" className="page-link">3</a></li>
-                                <li> <a href="#" className="page-link">4</a></li>
-                                <li> <a href="#" className="page-link">5</a></li>
-                                <li> <a href="#" className="page-link">6</a></li>
-                                <li> <a href="#" className="page-link">7</a></li>
-                                <li> <a href="#" className="page-link">8</a></li>
-                                <li> <a href="#" className="page-link">9</a></li>
-                                <li> <a href="#" className="page-link">10</a></li>
-                            <li className="page-item" ><a className="page-link">다음</a></li>
+                            <li className="page-item"><a className="page-link">이전</a></li>
+                            <li><a href="#" className="page-link">1</a></li>
+                            <li><a href="#" className="page-link">2</a></li>
+                            <li><a href="#" className="page-link">3</a></li>
+                            <li><a href="#" className="page-link">4</a></li>
+                            <li><a href="#" className="page-link">5</a></li>
+                            <li><a href="#" className="page-link">6</a></li>
+                            <li><a href="#" className="page-link">7</a></li>
+                            <li><a href="#" className="page-link">8</a></li>
+                            <li><a href="#" className="page-link">9</a></li>
+                            <li><a href="#" className="page-link">10</a></li>
+                            <li className="page-item"><a className="page-link">다음</a></li>
                         </ul>
                     </td>
                 </tr>
