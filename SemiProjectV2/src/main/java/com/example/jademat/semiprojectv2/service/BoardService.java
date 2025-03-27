874bcd8d5070ca986f8b031afc061dc19ada216b
jademat <01088049128a@gmail.com>
2025-03-27 12:03:42 +0900

    JPA Pageable로 구현한 게시판 검색 API

--- SemiProjectV2/src/main/java/com/example/jademat/semiprojectv2/service/BoardService.java
+++ SemiProjectV2/src/main/java/com/example/jademat/semiprojectv2/service/BoardService.java
@@ -1,11 +1,17 @@
 package com.example.jademat.semiprojectv2.service;
 
 import com.example.jademat.semiprojectv2.domain.Board;
+import com.example.jademat.semiprojectv2.domain.BoardDTO;
 import com.example.jademat.semiprojectv2.domain.BoardListDTO;
+import org.springframework.data.domain.Page;
 
 public interface BoardService {
 
     Board newBoard(Board board);
 
     BoardListDTO readBoard(int cpg);
+
+    BoardListDTO findBoard(int cpg, String findtype, String findkey);
+
+    Page<BoardDTO> testReadBoard(int cpg);
 }
