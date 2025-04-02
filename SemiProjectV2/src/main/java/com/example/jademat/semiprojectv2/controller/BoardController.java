53c39e3f6d18de179ed4e6fb7167ae6766a23a0c
jademat <01088049128a@gmail.com>
2025-04-02 14:12:41 +0900

    next.js board/list,write,view 추가

--- SemiProjectV2/src/main/java/com/example/jademat/semiprojectv2/controller/BoardController.java
+++ SemiProjectV2/src/main/java/com/example/jademat/semiprojectv2/controller/BoardController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {"http://localhost:5173","http://127.0.0.1:3000"})
 @RestController
 @RequestMapping("/api/board/")
 @RequiredArgsConstructor
