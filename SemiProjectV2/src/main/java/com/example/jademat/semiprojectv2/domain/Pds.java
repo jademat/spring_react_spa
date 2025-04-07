7f2437f6951f0e3d8de47e977c1012c1960ad48f
jademat <01088049128a@gmail.com>
2025-04-07 10:29:43 +0900

    .

---
+++ SemiProjectV2/src/main/java/com/example/jademat/semiprojectv2/domain/Pds.java
@@ -0,0 +1,52 @@
+package com.example.jademat.semiprojectv2.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+@Table(name="pds3")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Pds {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int pno;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String userid;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String contents;
+
+    @Column(nullable = false)
+    private int thumbs = 0;
+
+    @Column(nullable = false)
+    private int views = 0;
+
+    @CreationTimestamp
+    private LocalDateTime regdate;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pno")
+    private List<PdsAttach> pdsAttach = new ArrayList<>();
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pno")
+    private List<PdsReply> pdsReply = new ArrayList<>();
+
+}
---
+++ SemiProjectV2/src/main/java/com/example/jademat/semiprojectv2/repository/PdsAttachRepository.java
@@ -0,0 +1,9 @@
+package com.example.jademat.semiprojectv2.repository;
+
+
+import com.example.jademat.semiprojectv2.domain.PdsAttach;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PdsAttachRepository extends JpaRepository<PdsAttach, Long> {
+
+}
