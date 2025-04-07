a9ec09db27d6033f4c659b33673d7ed562fa3164
jademat <01088049128a@gmail.com>
2025-04-07 10:29:52 +0900

    .

---
+++ SemiProjectV2/src/main/java/com/example/jademat/semiprojectv2/repository/PdsReplyRepository.java
@@ -0,0 +1,9 @@
+package com.example.jademat.semiprojectv2.repository;
+
+
+import com.example.jademat.semiprojectv2.domain.PdsReply;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PdsReplyRepository extends JpaRepository<PdsReply, Long> {
+
+}
---
+++ SemiProjectV2/src/main/java/com/example/jademat/semiprojectv2/repository/PdsRepository.java
@@ -0,0 +1,9 @@
+package com.example.jademat.semiprojectv2.repository;
+
+
+import com.example.jademat.semiprojectv2.domain.Pds;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PdsRepository extends JpaRepository<Pds, Long> {
+
+}
