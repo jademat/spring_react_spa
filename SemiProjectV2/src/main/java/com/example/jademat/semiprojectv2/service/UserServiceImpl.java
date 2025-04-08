57ddb4db630907e287e083c1056adb3d79a1abdb
jademat <01088049128a@gmail.com>
2025-04-08 09:46:59 +0900

    이메일 인증 발송을 위한 UserService 수정

--- SemiProjectV2/src/main/java/com/example/jademat/semiprojectv2/service/UserServiceImpl.java
+++ SemiProjectV2/src/main/java/com/example/jademat/semiprojectv2/service/UserServiceImpl.java
@@ -3,7 +3,11 @@ package com.example.jademat.semiprojectv2.service;
 import com.example.jademat.semiprojectv2.domain.User;
 import com.example.jademat.semiprojectv2.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +15,8 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JavaMailSender mailSender;
 
     @Override
     public User newUser(User user) {
@@ -23,9 +29,31 @@ public class UserServiceImpl implements UserService {
         // 이메일 중복 체크
         if (userRepository.existsByEmail(user.getEmail())) {
             throw new IllegalStateException("이미 존재하는 이메일입니다!!");
+            try {
+                user.setVerifycode(makeVerifyCode());   // 계정에 인증코드 생성
+                sendVerifyCode(user);                   // 메일로 확인용 인증코드 발송
+            } catch (Exception e){
+                e.printStackTrace();
+                throw new IllegalStateException("인증코드 발송중 문제 발생");
+            }
         }
-
+        user.serPasswd(passwordEncoder.encode(user.getPasswd()));       // 비밀번호를 암호화시켜 저장
         return userRepository.save(user);
+
+    }
+
+    // 알파벳과 숫자로 구성된 6자리 코드 생성
+    private String makeVerifyCode() {
+        return RandomStringUtils.randomAlphanumeric(6);
+    }
+
+    // 인증코드가 포함된 메일 발송
+    private void sendVerifyCode(User user) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(user.getEmail());
+        message.setSubject("회원가입 이메일 인증 코드 발송");
+        message.setText("인증코드 : " + user.getVerifycode());
+        mailSender.send(message);
 
     }
 
